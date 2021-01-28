c46d0f02cb05866404a77bc097019d16d07e0055
inae <bbaaginae@gmail.com>
2021-01-28 19:58:45 +0900

    이미지, 폰트, 데이터 로더 추가

--- webpack.config.js
+++ webpack.config.js
@@ -45,6 +45,22 @@ module.exports = (env, options) => {
             {loader: 'style-loader'},
             {loader: 'css-loader'}
           ]
+        },
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.(csv|tsv)$/i,
+          use: ['csv-loader'],
+        },
+        {
+          test: /\.xml$/i,
+          use: ['xml-loader'],
         }
       ],
     },
